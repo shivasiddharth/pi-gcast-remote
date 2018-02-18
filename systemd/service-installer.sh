f3790c61b6ccdd533219dd8e0a547ec76d4a50c1
shivasiddharth <ushivasiddharth@gmail.com>
2018-02-18 01:21:31 +0000

    Update and rename service-installer.sh to systemd/service-installer.sh

---
+++ systemd/service-installer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+set -o errexit
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root (use sudo)" 1>&2
+   exit 1
+fi
+
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+repo_path="$PWD"
+
+for service in systemd/*.service; do
+  sed "s:/home/pi/pi-gcast-remote:${repo_path}:g" "$service" \
+    > "/lib/systemd/system/$(basename "$service")"
+done
